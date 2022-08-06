51cf5aa4fe8532d8a52ae02e44325f6c5a6b65c9
saifur1681 <moonly1681@gmail.com>
2022-08-06 21:38:51 +0600

    private route addeded

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
+import Orders from './Components/Orders/Orders';
 import Products from './Components/Products/Products';
 import Register from './Components/Register/Register';
+import RequireAuth from './Components/RequireAuth/RequireAuth';
+
+
 
 function App() {
+  
   return (
     <div className="App">
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>} >Home</Route>
-        <Route path="/products" element={<Products></Products>} ></Route>
+        <Route path="/products" element={
+          <RequireAuth>
+            <Products></Products>
+          </RequireAuth>
+        } ></Route>
+        <Route path="/orders" element={
+          <RequireAuth>
+            <Orders></Orders>
+          </RequireAuth>
+        } ></Route>
         <Route path="/register" element={<Register></Register>}></Route>
         <Route path="/login" element={<Login></Login>} >Login</Route>
       </Routes>
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,5 +1,6 @@
 import { getAuth } from 'firebase/auth';
 import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom'
 
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import app from '../../Firebase-init';
@@ -7,15 +8,26 @@ import app from '../../Firebase-init';
 
 const auth = getAuth(app);
 
+
 const Login = () => {
 
-    const [SignInWithGoogle, user] = useSignInWithGoogle(auth)
+    const [SignInWithGoogle, user] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location?.state?.from?.pathname || "/";
+
+    const handleGoogleSignIn = () => {
+        SignInWithGoogle()
+            .then(() => {
+                navigate(from, { replace: true });
+            })
+    }
 
     return (
         <div>
             <h2>Please Login</h2>
             <div style={{ margin: "20px" }}>
-                <button onClick={() => SignInWithGoogle()} >Google sign in</button>
+                <button onClick={handleGoogleSignIn} >Google sign in</button>
             </div>
             <form>
                 <input type="email" placeholder='Your Email' />
